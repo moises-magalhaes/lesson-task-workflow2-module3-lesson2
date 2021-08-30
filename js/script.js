359b6ac84676ffc5f2a0489c0b6205058c851c6d
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2021-08-30 14:48:49 -0300

    axios

--- js/script.js
+++ js/script.js
@@ -1,44 +1,84 @@
-async function postRequest() {
-	const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-		method: "POST",
-		body: JSON.stringify({
-			title: "foo",
-			body: "bar",
-			userId: 1,
-		}),
-		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-		},
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/posts/";
+
+//Axios post method
+
+async function AxiosPostRequest() {
+	const response = await axios.post(url, {
+		title: "foot",
+		body: "bart",
+		userId: 1,
 	});
-	const json = await response.json();
-	console.log(json);
+
+	console.log(response.data);
 }
 
-postRequest();
+AxiosPostRequest();
 
-async function putRequest() {
-	const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-		method: "PUT",
-		body: JSON.stringify({
-			id: 1,
-			title: "foot",
-			body: "bart",
-			userId: 1,
-		}),
-		headers: {
-			"Content-type": "application/json; charset=UTF-8",
-		},
+async function AxiosPutRequest() {
+	//Axios put method
+	const response = await axios.put(url + 1, {
+		id: 1,
+		title: "foot",
+		body: "bart",
+		userId: 1,
 	});
-	const json = await response.json();
-	console.log(json);
+	console.log(response.data);
 }
 
-putRequest();
+AxiosPutRequest();
 
-function deleteRequest() {
-	fetch("https://jsonplaceholder.typicode.com/posts/1", {
-		method: "DELETE",
-	});
+function AxiosDeleteRequest() {
+	axios.delete(url + 1);
 }
 
-deleteRequest();
+AxiosDeleteRequest();
+
+// async function postRequest() {
+// 	// url used
+// 	const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+// 		method: "POST",
+// 		body: JSON.stringify({
+// 			title: "foo",
+// 			body: "bar",
+// 			userId: 1,
+// 		}),
+// 		headers: {
+// 			"Content-type": "application/json; charset=UTF-8",
+// 		},
+// 	});
+// 	const json = await response.json();
+// 	console.log(json);
+// }
+
+// postRequest();
+
+// // using fetch
+// async function putRequest() {
+// 	// GET is the default method
+// 	const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+// 		method: "PUT",
+// 		body: JSON.stringify({
+// 			id: 1,
+// 			title: "foot",
+// 			body: "bart",
+// 			userId: 1,
+// 		}),
+// 		headers: {
+// 			"Content-type": "application/json; charset=UTF-8",
+// 		},
+// 	});
+// 	const json = await response.json();
+// 	console.log(json);
+// }
+
+// putRequest();
+
+// function deleteRequest() {
+// 	fetch("https://jsonplaceholder.typicode.com/posts/1", {
+// 		method: "DELETE",
+// 	});
+// }
+
+// deleteRequest();
